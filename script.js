3bb16c5e9f479af100c48409fbd3d2f80487b6dd
Akramkhan1990 <acconsultant1234@gmail.com>
2025-08-20 08:57:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
 
-function SportsCar(make, model, topSpeed) {}
+}
+car.prototype.getMakeModel=function() {
+	return '${this.make},{this.model}';
+	
+};
+function SportsCar(make, model, topSpeed) {
+	car.call(this,Make,model,);
+	this.topspeed=topspeed;
+}
+SportsCar.prototype=oject.create(car.prototype);
+SportsCar.prototype.constructor=SportsCar;
+SportsCar.prototype.accelerat=function(){
+	console.loge('${this.make} ${this.model} ${this.topspeed}mph');
+	
+	
+};
+const maycar=new car("Toyota","corolla")
+console.log(myrcar.getmodel());
+const sportscar=new sportscar("toyota","corolla"200);
+console.log(sportscar.getmodel());
 
 // Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+window.car=car;
+window.SportsCar =SportsCar;
+
